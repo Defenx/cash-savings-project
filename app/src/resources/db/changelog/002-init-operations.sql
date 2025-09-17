b5a3b5e6552cfc252ddb8aec3d871312348143fc
SVTsygankov <s.v.tsygankov@mail.ru>
2025-09-17 10:04:16 +0500

    Изменено поле created_date на timestamptz"

--- app/src/resources/db/changelog/002-init-operations.sql
+++ app/src/resources/db/changelog/002-init-operations.sql
@@ -1,8 +1,11 @@
+--liquibase formatted sql
+
+--changeset svtsygankov:1
 CREATE TABLE operations (
                             id UUID NOT NULL,
                             account_id UUID NOT NULL,
                             date DATE NOT NULL,
-                            created_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+                            created_date TIMESTAMP WITH TIME ZONE NOT NULL,
                             description TEXT,
                             amount NUMERIC(19,2) NOT NULL,
                             currency TEXT NOT NULL,
@@ -13,3 +16,4 @@ ALTER TABLE operations
     ADD CONSTRAINT fk_operations_account
         FOREIGN KEY (account_id)
             REFERENCES accounts(id);
+--rollback DROP TABLE operations
