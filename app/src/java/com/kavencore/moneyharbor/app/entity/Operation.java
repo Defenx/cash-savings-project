319df5433efee8c93c1abdc99729086d8ef48a37
SVTsygankov <s.v.tsygankov@mail.ru>
2025-09-17 15:04:44 +0500

    Реализовать Entity исходя operation. Добавлено поле operations в Account.

---
+++ app/src/java/com/kavencore/moneyharbor/app/entity/Operation.java
@@ -0,0 +1,86 @@
+package com.kavencore.moneyharbor.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "operations")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(onlyExplicitlyIncluded = true)
+public class Operation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @ToString.Include
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "account_id", nullable = false)
+    @ToString.Include
+    private Account account;
+
+    @ToString.Include
+    private LocalDate date;
+
+    private OffsetDateTime createdDate;
+
+    private String description;
+
+    @ToString.Include
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @ToString.Include
+    private Currency currency;
+
+    @PrePersist
+    void onCreate() {
+        if (createdDate == null) {
+            createdDate = OffsetDateTime.now();
+        }
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Operation operation = (Operation) o;
+        return getId() != null && Objects.equals(getId(), operation.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
+                : getClass().hashCode();
+    }
+}
