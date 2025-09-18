387c46a3999baa2aeb977cec11e0635a676d72a6
AndreyTerex <katazheh@gmail.com>
2025-09-18 10:22:05 +0300

    Добавлен liquibase скрипт для таблицы category с уникальным индексом по (user_id, name, type) Добавил комментарии с метаинформацией для liquibase в файлы созданные ранее.

--- app/src/resources/db/changelog/001-init-accounts.sql
+++ app/src/resources/db/changelog/001-init-accounts.sql
@@ -1,6 +1,11 @@
+--liquibase formatted sql
+
+--changeset andreyterex:1
+--comment: создание таблицы accounts
 CREATE TABLE accounts (
                           id UUID PRIMARY KEY NOT NULL,
                           title TEXT NOT NULL,
                           currency TEXT NOT NULL,
                           amount NUMERIC(19,2) NOT NULL
 );
+--rollback DROP TABLE accounts
