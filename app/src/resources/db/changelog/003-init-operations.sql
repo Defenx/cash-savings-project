ea91b24834425fbb0d23dafa797a518f62cd7cb7
SVTsygankov <s.v.tsygankov@mail.ru>
2025-09-25 14:14:57 +0500

    Промежуточный1

---
+++ app/src/resources/db/changelog/003-init-operations.sql
@@ -0,0 +1,19 @@
+--liquibase formatted sql
+
+--changeset svtsygankov:1
+CREATE TABLE operations (
+                            id UUID NOT NULL,
+                            account_id UUID NOT NULL,
+                            date DATE NOT NULL,
+                            created_date TIMESTAMP WITH TIME ZONE NOT NULL,
+                            description TEXT,
+                            amount NUMERIC(19,2) NOT NULL,
+                            currency TEXT NOT NULL,
+                            PRIMARY KEY (id)
+);
+
+ALTER TABLE operations
+    ADD CONSTRAINT fk_operations_account
+        FOREIGN KEY (account_id)
+            REFERENCES accounts(id);
+--rollback DROP TABLE operations
