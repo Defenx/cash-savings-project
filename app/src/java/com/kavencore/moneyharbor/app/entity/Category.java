77c7b0c21bf6a89eae97613617b64de09d640e95
SVTsygankov <s.v.tsygankov@mail.ru>
2025-09-29 21:39:48 +0500

    Удалено uniqueConstraints из сущности Category

--- app/src/java/com/kavencore/moneyharbor/app/entity/Category.java
+++ app/src/java/com/kavencore/moneyharbor/app/entity/Category.java
@@ -10,7 +10,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,9 +22,7 @@ import java.util.Objects;
 import java.util.UUID;
 
 @Entity
-@Table(name = "categories", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"user_id", "name", "type"})
-})
+@Table(name = "categories")
 @Getter
 @Setter
 @Builder
