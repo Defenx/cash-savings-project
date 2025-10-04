9a999e2fd5d496c8d6af13037e873c76f1acb772
AllaVrublevskaja <allavrublevskaja@yandex.ru>
2025-10-04 16:19:50 +0300

    OpenApiReader.java - Класс удален build.gradle.kts - Удалена зависимость testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")

--- component-test/build.gradle.kts
+++ component-test/build.gradle.kts
@@ -22,7 +22,6 @@ dependencies {
     testImplementation(libs.spring.boot.testcontainers)
     testImplementation(libs.testcontainers.junit)
     testImplementation(libs.testcontainers.postgresql)
-    testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
 
     testImplementation(project(":app"))
 
