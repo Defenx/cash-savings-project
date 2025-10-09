bdd0768e6dbd055a92b3e076e8518dac2c676c03
SVTsygankov <s.v.tsygankov@mail.ru>
2025-10-09 11:30:45 +0500

    Убраны не используемые импорты

--- component-test/src/test/java/AccountsComponentTest.java
+++ component-test/src/test/java/AccountsComponentTest.java
@@ -1,6 +1,4 @@
-import com.jayway.jsonpath.JsonPath;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
