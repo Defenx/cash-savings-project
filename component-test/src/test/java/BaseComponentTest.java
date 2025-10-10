54417dc76831849290bc01ff02cd5d6a10a3e33f
AllaVrublevskaja <allavrublevskaja@yandex.ru>
2025-10-10 15:53:50 +0300

    исправила название метода

--- component-test/src/test/java/BaseComponentTest.java
+++ component-test/src/test/java/BaseComponentTest.java
@@ -85,7 +85,7 @@ public abstract class BaseComponentTest {
                 .accept(MediaType.APPLICATION_JSON_VALUE));
     }
 
-    protected ResultActions UPDATE_TITLE_NULL(String path, String json) throws Exception {
+    protected ResultActions performPatchAuth(String path, String json) throws Exception {
         return mvc.perform(MockMvcRequestBuilders.patch(path)
                 .with(httpBasic(ACCOUNT_TEST_EMAIL, TEST_PASSWORD))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
